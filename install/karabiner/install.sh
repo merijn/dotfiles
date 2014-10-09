0dcab51c1773402161ac6226c86a50b7f2742da7
Merijn Verstraaten <merijn@inconsistent.nl>
2014-10-09 00:33:09 -0700

    Add Karabiner custom keybindings and installer.

---
+++ install/karabiner/install.sh
@@ -0,0 +1,14 @@
+if [ "$(uname)" != "Darwin" ]; then
+    # Don't try to install on non-OSX
+    return 0
+fi
+
+karabiner_dir="${HOME}/Library/Application Support/Karabiner/"
+
+if [ ! -d "${karabiner_dir}" ]; then
+    printf "%s\n" "Karabiner config directory not found, skipping custom "\
+           "binding installation." 1>&2
+    return 0
+fi
+
+install_file "${src}/private.xml" "${karabiner_dir}/private.xml"
