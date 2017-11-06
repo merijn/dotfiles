7b9f5ec61a059dad110f9bbb30a845490fd92f41
Merijn Verstraaten <merijn@inconsistent.nl>
2017-11-06 14:15:23 +0100

    Fix bitbucket push hook.

--- dotfiles/hg-data/hooks/push-hook.py
+++ dotfiles/hg-data/hooks/push-hook.py
@@ -1,9 +1,15 @@
 from os import chdir, system
 
+def stripPrefix(s, prefix):
+    n = 0
+    if s.startswith(prefix):
+        n = len(prefix)
+    return s[n:]
+
 def bitbucket(ui, repo, args=None, **kwargs):
     url = ui.paths["default"].url
     if args == "push" and url.host == "github.com" and "bitbucket" in ui.paths:
         if repo.url().startswith("file:"):
-            chdir(repo.url().strip("file:"))
+            chdir(stripPrefix(repo.url(), "file:"))
             system("hg push bitbucket")
     return False
