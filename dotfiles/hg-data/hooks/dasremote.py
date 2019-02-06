fa07f8ce17f7bb1e692066eeec6c2a6b4cebc5bb
Merijn Verstraaten <merijn@inconsistent.nl>
2019-02-06 11:38:04 +0100

    Fix das-remote hooks.

--- dotfiles/hg-data/hooks/dasremote.py
+++ dotfiles/hg-data/hooks/dasremote.py
@@ -10,8 +10,9 @@ def dasremote(ui, repo, args=None, **kwargs):
         path = ui.expandpath(path, "default-push")
 
     path = ui.expandpath(path, "default")
-    if path and urlparse(path).hostname.startswith("das"):
-        kwargs["opts"]["remotecmd"] = "/home/mverstra/opt/bin/hg"
-    elif path and urlparse(path).hostname.startswith("astron"):
-        kwargs["opts"]["remotecmd"] = "/home/mverstra/opt/bin/hg"
+    if path and urlparse(path).hostname:
+        if urlparse(path).hostname.startswith("das"):
+            kwargs["opts"]["remotecmd"] = "/home/mverstra/opt/bin/hg"
+        elif urlparse(path).hostname.startswith("astron"):
+            kwargs["opts"]["remotecmd"] = "/home/mverstra/opt/bin/hg"
     return False
