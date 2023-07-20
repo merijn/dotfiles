591f564b9353dc292cccc67586ab4f561e2bb5b9
Merijn Verstraaten <merijn@inconsistent.nl>
2023-07-20 14:49:16 +0200

    Fix usage of install_config to check for success.

--- install-osx/macports/install.sh
+++ install-osx/macports/install.sh
@@ -15,26 +15,27 @@ if ! [[ -d "$notify_dir" ]]; then
 fi
 
 # Copy over the relevant shell scripts
-install_config "$PWD/install-osx/macports/macports-update" \
+if install_config "$PWD/install-osx/macports/macports-update" \
                 "$notify_dir/macports-update"
+then
+    # If growlnotify is present, install the notification job and load it into
+    # launchd.
+    if command -v growlnotify 2>/dev/null; then
+        # If a job is loaded AND a new plist was installed, unload the old job
+        # before loading the new one.
+        if sudo_install_config \
+                "$PWD/install-osx/macports/com.dotfiles.macports.plist" \
+                "/Library/LaunchDaemons/com.dotfiles.macports.plist" \
+            && list_loaded com.dotfiles.macports
+        then
+            sudo launchctl remove com.dotfiles.macports
+        fi
 
-# If growlnotify is present, install the notification job and load it into
-# launchd.
-if hash growlnotify 2>/dev/null; then
-    # If a job is loaded AND a new plist was installed, unload the old job
-    # before loading the new one.
-    if sudo_install_config \
-            "$PWD/install-osx/macports/com.dotfiles.macports.plist" \
-            "/Library/LaunchDaemons/com.dotfiles.macports.plist" \
-        && list_loaded com.dotfiles.macports
-    then
-        sudo launchctl remove com.dotfiles.macports
+        # If no plist is loaded, load the new plist.
+        if ! sudo_list_loaded com.dotfiles.macports 2>/dev/null; then
+            sudo launchctl load /Library/LaunchDaemons/com.dotfiles.macports.plist
+        fi
+    else
+        printf "growlnotify not found. Auto-updating not enabled.\n" 1>&2
     fi
-
-    # If no plist is loaded, load the new plist.
-    if ! sudo_list_loaded com.dotfiles.macports 2>/dev/null; then
-        sudo launchctl load /Library/LaunchDaemons/com.dotfiles.macports.plist
-    fi
-else
-    printf "MacPorts not found. Auto-updating not enabled.\n" 1>&2
 fi
