c3b32bb44b38a2ee25abb82785fc2d1ac3522302
Merijn Verstraaten <merijn@inconsistent.nl>
2023-07-25 14:22:54 +0200

    Homebrew update logic based on macports. Fixes #33.

--- install-osx/homebrew/install.sh
+++ install-osx/homebrew/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 notify_dir="/Library/Application Support/Dotfiles"
 
-if ! [[ -f /opt/local/bin/port ]]; then
-    printf "MacPorts not found, skipping update script install.\n" 1>&2
+if ! [[ -f /usr/local/bin/brew ]]; then
+    printf "HomeBrew not found, skipping update script install.\n" 1>&2
     return 0
 fi
 
@@ -15,8 +15,8 @@ if ! [[ -d "$notify_dir" ]]; then
 fi
 
 # Copy over the relevant shell scripts
-if install_config "$PWD/install-osx/macports/macports-update" \
-                "$notify_dir/macports-update"
+if install_config "$PWD/install-osx/homebrew/homebrew-update" \
+                "$notify_dir/homebrew-update"
 then
     # If growlnotify is present, install the notification job and load it into
     # launchd.
@@ -24,16 +24,16 @@ then
         # If a job is loaded AND a new plist was installed, unload the old job
         # before loading the new one.
         if sudo_install_config \
-                "$PWD/install-osx/macports/com.dotfiles.macports.plist" \
-                "/Library/LaunchDaemons/com.dotfiles.macports.plist" \
-            && sudo_list_loaded com.dotfiles.macports
+                "$PWD/install-osx/homebrew/com.dotfiles.homebrew.plist" \
+                "/Library/LaunchDaemons/com.dotfiles.homebrew.plist" \
+            && sudo_list_loaded com.dotfiles.homebrew
         then
-            sudo launchctl remove com.dotfiles.macports
+            sudo launchctl remove com.dotfiles.homebrew
         fi
 
         # If no plist is loaded, load the new plist.
-        if ! sudo_list_loaded com.dotfiles.macports 2>/dev/null; then
-            sudo launchctl load /Library/LaunchDaemons/com.dotfiles.macports.plist
+        if ! sudo_list_loaded com.dotfiles.homebrew 2>/dev/null; then
+            sudo launchctl load /Library/LaunchDaemons/com.dotfiles.homebrew.plist
         fi
     else
         printf "growlnotify not found. Auto-updating not enabled.\n" 1>&2
